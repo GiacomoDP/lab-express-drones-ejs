9e3bae0ae77d86bfab143ec9fb47dec48f837048
Giacomo De Pol <giacomo.depol@hotmail.it>
2022-05-30 18:07:01 +0200

    third iteration

--- routes/drones.js
+++ routes/drones.js
@@ -2,20 +2,37 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+//const mongoose = require("mongoose")
+const Drone = require("../models/Drone.model")
+//const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones-ejs";
+
+
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+  .then((drones)=>{
+    console.log("drones onformations", drones)
+    res.render("./drones/list", {drones})
+  })
+  .catch(error => {console.log(error)})
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form")
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  try{
+    const newDrone = await Drone.create(req.body)
+    console.log('new drone created:', newDrone)
+    res.redirect("/drones")
+  }catch (err){
+    console.log("something went wrong:", err)
+    res.render("drones/create-form")
+  }
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,24 @@
 // Iteration #1
+const { default: mongoose } = require("mongoose");
+
+require("../db")
+const droneModel = require("../models/Drone.model");
+
+const drones = [
+    { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+  ];
+
+  droneModel.insertMany(drones)
+  .then((allDronesDB) => {
+      console.log("All drones seeded!", allDronesDB);
+      return mongoose.connection.close();
+  })
+  .then(() => {
+      console.log("Db closed");
+  })
+
+  .catch((err) => {
+      console.log("there is a problem with seeding", err);
+  })
